699d7e70fc267fa5c47bed5494148d36e26c5aea
h19921012 <xh894@nyu.edu>
2017-02-18 16:56:21 -0500

    replace global variables with local ones

--- nbody_opt.py
+++ nbody_opt.py
@@ -11,6 +11,44 @@
 
 
 '''
+PI = 3.14159265358979323
+SOLAR_MASS = 4 * PI * PI
+DAYS_PER_YEAR = 365.24
+
+BODIES = {
+    'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
+
+    'jupiter': ([4.84143144246472090e+00,
+                 -1.16032004402742839e+00,
+                 -1.03622044471123109e-01],
+                [1.66007664274403694e-03 * DAYS_PER_YEAR,
+                 7.69901118419740425e-03 * DAYS_PER_YEAR,
+                 -6.90460016972063023e-05 * DAYS_PER_YEAR],
+                9.54791938424326609e-04 * SOLAR_MASS),
+
+    'saturn': ([8.34336671824457987e+00,
+                4.12479856412430479e+00,
+                -4.03523417114321381e-01],
+               [-2.76742510726862411e-03 * DAYS_PER_YEAR,
+                4.99852801234917238e-03 * DAYS_PER_YEAR,
+                2.30417297573763929e-05 * DAYS_PER_YEAR],
+               2.85885980666130812e-04 * SOLAR_MASS),
+
+    'uranus': ([1.28943695621391310e+01,
+                -1.51111514016986312e+01,
+                -2.23307578892655734e-01],
+               [2.96460137564761618e-03 * DAYS_PER_YEAR,
+                2.37847173959480950e-03 * DAYS_PER_YEAR,
+                -2.96589568540237556e-05 * DAYS_PER_YEAR],
+               4.36624404335156298e-05 * SOLAR_MASS),
+
+    'neptune': ([1.53796971148509165e+01,
+                 -2.59193146099879641e+01,
+                 1.79258772950371181e-01],
+                [2.68067772490389322e-03 * DAYS_PER_YEAR,
+                 1.62824170038242295e-03 * DAYS_PER_YEAR,
+                 -9.51592254519715870e-05 * DAYS_PER_YEAR],
+                5.15138902046611451e-05 * SOLAR_MASS)}
 #use set to reduce loop overhead
 my_set=set({('jupiter', 'neptune'),
            ('jupiter', 'saturn'),
@@ -24,7 +62,7 @@ my_set=set({('jupiter', 'neptune'),
            ('sun', 'uranus')})
 
 
-def advance(dt,iterations):
+def advance(dt,iterations,bodies=BODIES):
     '''
         advance the system one timestep
     '''
@@ -54,7 +92,7 @@ def advance(dt,iterations):
             r[2] += dt * vz
 
     
-def report_energy(e=0.0):
+def report_energy(e=0.0,bodies=BODIES):
     '''
         compute the energy and return it so that it can be printed
     '''
@@ -80,7 +118,7 @@ def report_energy(e=0.0):
 
 
 
-def nbody(loops, reference, iterations,px=0.0, py=0.0, pz=0.0):
+def nbody(loops, reference, iterations,px=0.0, py=0.0, pz=0.0,bodies=BODIES):
     '''
         nbody simulation
         loops - number of loops to run
@@ -107,44 +145,7 @@ def nbody(loops, reference, iterations,px=0.0, py=0.0, pz=0.0):
 
 if __name__ == '__main__':
     
-    PI = 3.14159265358979323
-    SOLAR_MASS = 4 * PI * PI
-    DAYS_PER_YEAR = 365.24
-
-    BODIES = {
-        'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
-
-        'jupiter': ([4.84143144246472090e+00,
-                     -1.16032004402742839e+00,
-                     -1.03622044471123109e-01],
-                    [1.66007664274403694e-03 * DAYS_PER_YEAR,
-                     7.69901118419740425e-03 * DAYS_PER_YEAR,
-                     -6.90460016972063023e-05 * DAYS_PER_YEAR],
-                    9.54791938424326609e-04 * SOLAR_MASS),
-
-        'saturn': ([8.34336671824457987e+00,
-                    4.12479856412430479e+00,
-                    -4.03523417114321381e-01],
-                   [-2.76742510726862411e-03 * DAYS_PER_YEAR,
-                    4.99852801234917238e-03 * DAYS_PER_YEAR,
-                    2.30417297573763929e-05 * DAYS_PER_YEAR],
-                   2.85885980666130812e-04 * SOLAR_MASS),
-
-        'uranus': ([1.28943695621391310e+01,
-                    -1.51111514016986312e+01,
-                    -2.23307578892655734e-01],
-                   [2.96460137564761618e-03 * DAYS_PER_YEAR,
-                    2.37847173959480950e-03 * DAYS_PER_YEAR,
-                    -2.96589568540237556e-05 * DAYS_PER_YEAR],
-                   4.36624404335156298e-05 * SOLAR_MASS),
-
-        'neptune': ([1.53796971148509165e+01,
-                     -2.59193146099879641e+01,
-                     1.79258772950371181e-01],
-                    [2.68067772490389322e-03 * DAYS_PER_YEAR,
-                     1.62824170038242295e-03 * DAYS_PER_YEAR,
-                     -9.51592254519715870e-05 * DAYS_PER_YEAR],
-                    5.15138902046611451e-05 * SOLAR_MASS)}
+    
 
     import timeit
     print(timeit.timeit(lambda:nbody(100, 'sun', 20000),number=1))
